8f0ca5958cc585d08bed2dd4bcccf28abf5e7a04
takanaaaa <se-ha_1v4@docomo.ne.jp>
2022-03-01 20:13:02 +0900

    [Finish]quiz

---
+++ javascript-app/MyQuizApp/js/main.js
@@ -0,0 +1,74 @@
+'use strict'
+
+{
+  const question = document.getElementById('question');
+  const choices = document.getElementById('choices');
+  const btn = document.getElementById('btn');
+
+
+  const quizSet = [
+    {q: 'What is A?', c: ['A0', 'A1', 'A2']},
+    {q: 'What is B?', c: ['B0', 'B1', 'B2']},
+    {q: 'What is C?', c: ['C0', 'C1', 'C2']},
+  ];
+  let currentNum = 0; // 0のクイズから開始
+  let isAnswered;
+
+  function shuffle(arr) {
+    // i = 最後の要素
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); // ランダムにシャッフルする要素 0からi番目
+      [arr[j], arr[i]] = [arr[i], arr[j]]; // 分割代入
+    }
+    return arr;
+  }
+
+  function checkAnswer(li) {
+    if (isAnswered) {
+      return;
+    };
+    isAnswered =true;
+
+    if(li.textContent === quizSet[currentNum].c[0]) {
+      li.classList.add('correct');
+    } else {
+      li.classList.add('wrong');
+    }
+
+    btn.classList.remove('disabled')
+  }
+
+  function setQuiz() {
+    isAnswered = false;
+    question.textContent = quizSet[currentNum].q;
+
+    while(choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
+
+    const shuffleChoices = shuffle([...quizSet[currentNum].c]); // スプレッド演算子
+    shuffleChoices.forEach(choice => {
+      const li = document.createElement('li');
+      li.textContent = choice;
+      li.addEventListener('click', () => {
+        checkAnswer(li);
+      })
+      choices.appendChild(li);
+    });
+
+    if (currentNum === quizSet.length - 1) {
+      btn.textContent = 'Show Score'
+    }
+  }
+
+  setQuiz();
+
+  btn.addEventListener('click', () => {
+    if (btn.classList.contains('disabled')) {
+      return;
+    }
+    btn.classList.add('disabled');
+    currentNum++;
+    setQuiz();
+  });
+}
